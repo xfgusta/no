be0fa9b112e0465ef677c857db005b0032d89306
Gustavo Costa <gusta@null.net>
2020-09-02 19:18:35 -0300

    Convert tabs to spaces

--- no.c
+++ no.c
@@ -3,29 +3,29 @@
 
 // repeatedly output str until there is a error; then fail
 #define NO(str) {           \
-	while(puts(str) != EOF) \
-	 	continue;           \
-	return 1;               \
+    while(puts(str) != EOF) \
+        continue;           \
+    return 1;               \
 }
 
 int main(int argc, char **argv) {
-	// no string specified
-	if(argc == 1)
-		NO("n");
-	
-	// static malloc for line (specified string)
-	size_t len = 0;
-	for(size_t i = 1; i < argc; i++)
-		len += strlen(argv[i]) + 1;
-	char line[--len];
-	
-	// copy argv[1]..argv[argc-1] to line
-	for(size_t i = 1; i < argc; i++) {
-		strcat(line, argv[i]);
-		strcat(line, " ");
-	}
-	line[len] = 0;
-	
-	NO(line);
+    // no string specified
+    if(argc == 1)
+        NO("n");
+    
+    // static malloc for line (specified string)
+    size_t len = 0;
+    for(size_t i = 1; i < argc; i++)
+        len += strlen(argv[i]) + 1;
+    char line[--len];
+    
+    // copy argv[1]..argv[argc-1] to line
+    for(size_t i = 1; i < argc; i++) {
+        strcat(line, argv[i]);
+        strcat(line, " ");
+    }
+    line[len] = 0;
+    
+    NO(line);
 }
 
