239721d3a0e51c41da4d7bd246c92424308b13b1
Gustavo Costa <sirguxx@gmail.com>
2020-12-24 21:25:21 -0300

    Very minor update

--- no.c
+++ no.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
     if(argc == 1)
         while(1)
-            printf("n\n");
+            puts("n");
     while(1)
         for(int i = 1; i < argc; i++)
             printf("%s%c", argv[i], i+1 == argc ? '\n' : ' ');
